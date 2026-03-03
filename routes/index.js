be8fdfd6316eaf173f5bd359b062ac099d8d0cce
AnnabellePLS <annabellybutton44@gmail.com>
2026-03-03 10:01:03 -0700

    commit to main

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const routes = require("express").Router();
 const myController = require("../controllers");
 
 routes.get("/", myController.awesomeFunction);
-routes.get("/ttech", myController.getAllStudents);
+routes.get("/ttech", (req, res) => {
+    res.send('Tooele Tech is Awesome');
+});
 //student routes
 routes.use("/students", require("./students"))
 
